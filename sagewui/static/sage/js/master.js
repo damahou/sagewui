27f56cee09071826e98059e85903c23bcedab56d
J Miguel Farto <jmfarto@gmail.com>
2017-08-04 10:14:46 +0200

    cleanup: master.js

--- sagewui/static/sage/js/master.js
+++ sagewui/static/sage/js/master.js
@@ -2,21 +2,16 @@
 /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true */
 //"use strict";
 
-var replicate_str = function(x, n) {
-    var str = '';
-    for (var i = 0; i < n; ++i) {
-        str += x;
-    }
-};
-
 $(function () {
     var body = $('body'), body_id = body.attr('id');
 
     if (body.hasClass('active-worksheet')) {
         initialize_the_notebook();
-//'#worksheet_cell_list' why was it function (e) below.
-        $(document)
-            .on('click','#worksheet_cell_list .introspection .docstring .click-message', function (e) {
+        //'#worksheet_cell_list' why was it function (e) below.
+        $(document).on(
+            'click',
+            '#worksheet_cell_list .introspection .docstring .click-message',
+            function (e) {
                 var ds_elem = $(this).parent(), style;
 
                 var id = toint(ds_elem.parent().attr('id').slice(15));
